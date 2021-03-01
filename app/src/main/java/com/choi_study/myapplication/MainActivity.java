4d18c2359288fc994324ea1b26a4cbc308cb690f
ANVChoi <rnumch@naver.com>
2021-03-02 00:18:45 +0900

    YouTube Happy Note #4 진행 완료

--- app/src/main/java/com/choi_study/myapplication/MainActivity.java
+++ app/src/main/java/com/choi_study/myapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.choi_study.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseRemoteConfig remoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        remoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                //.setMinimumFetchIntervalInSeconds(3600) //해당소스는 Debug 모드일때만 사용할 것
+                //너무 인터벌이 자주 사용되면 프로젝트에 무리가 오므로
+                .build();
+        remoteConfig.setConfigSettingsAsync(configSettings);
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
+
+        remoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            boolean updated = task.getResult();
+                            Log.d("MainActivity", "Config params updated: " + updated);
+                            Toast.makeText(MainActivity.this, "Fetch and activate succeeded",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.e("new_app_version"," = " + remoteConfig.getLong("new_app_version"));
+                            Log.e("toolbar_img_count"," = " + remoteConfig.getLong("toolbar_img_count"));
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Fetch failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
